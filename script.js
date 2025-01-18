11ff88c3b6fa83bdcd6bb159bc24f67d78fb76f5
pixz <tinycactusking@gmail.com>
2025-01-18 01:35:48 -0500

    Add full functionality with result screen and game reset button

--- script.js
+++ script.js
@@ -84,6 +84,10 @@ const game = (function() {
         // flips the turn flag
         whosTurn = whosTurn === 'x' ? 'o' : 'x';
         winner = checkWinner();
+
+        if (winner != null) {
+            handleDOM.endGame();
+        }
     };
 
     const gameBoardToString = () => {
@@ -96,12 +100,20 @@ const game = (function() {
         `;
     }
 
+    const reset = () => {
+        winner = null;
+        whosTurn = 'x';
+        for (let i = 0; i < gameBoard.length; i++) {
+            gameBoard[i] = " ";
+        }
+    };
+
     // GETTERS
     const getWinner = () => winner;
     const getWhosTurn = () => whosTurn;
 
     // return object with all of the public properties and functions
-    return { gameBoard, flipTurn, gameBoardToString, getWinner, getWhosTurn };
+    return { gameBoard, flipTurn, gameBoardToString, getWinner, getWhosTurn, reset };
 })();
 
 function createPlayer(name, symbol) {
@@ -113,29 +125,81 @@ const handleDOM = (function() {
     // PUBLIC VARIABLES
     const cellList = document.querySelectorAll(".gameboard > div");
     // PRIVATE VARIABLES
+    let playerOne = {};
+    let playerTwo = {};
+    const startModal = document.querySelector("#new-game");
+    const gameBoardContainer = document.querySelector(".gameboard");
+    const endModal = document.querySelector("#end-game-results");
+    const gameOverText = document.querySelector("#gameover-text");
 
     // METHODS
     const updateCell = (cellNum) => {
         const symbolLetter = game.getWhosTurn();
         const symbol = document.createElement("span");
         symbol.textContent = symbolLetter.toUpperCase();
-        symbol.classList.add(symbolLetter.toUpperCase());
         cellList[cellNum].appendChild(symbol);
         game.gameBoard[cellNum] = symbolLetter;
     };
 
     const endGame = () => {
-        alert("The game is finished. " + game.getWinner() + " has won.")
+        gameBoardContainer.classList.add("hidden");
+        if (game.getWinner() == "tie") {
+            gameOverText.textContent = "Oh no the game ended in a tie!!";
+        }
+        else {
+            gameOverText.textContent = `${game.getWinner() === 'x' ? playerOne.name : playerTwo.name} is the winner!!`;
+        }
+        endModal.classList.remove("hidden");
     };
+
+    const startGame = () => {
+        // grab id of start modals
+        // display the board and hide the other modal
+        // grab player names
+        const playerOneNameInput = document.querySelector("#playerone-name");
+        const playerTwoNameInput = document.querySelector("#playertwo-name");
+
+        startModal.classList.add("hidden");
+        gameBoardContainer.classList.remove("hidden");
+
+        // default name for players is player1/2
+        let playerOneName = "Player 1";
+        let playerTwoName = "Player 2";
+
+        if (playerOneNameInput.value.trim() != "") {
+            playerOneName = playerOneNameInput.value;
+        }
+        if (playerTwoNameInput.value.trim() != "") {
+            playerTwoName = playerTwoNameInput.value;
+        }
+
+        playerOne = createPlayer(playerOneName, "x");
+        playerTwo = createPlayer(playerTwoName, "o");
+    };
+    const resetGame = () => {
+        // reset game board
+        // hide
+        // display start modal
+        game.reset();
+        for (const cell of cellList) {
+            if (cell.hasChildNodes()) {
+                cell.removeChild(cell.firstChild);
+            }
+        }
+        endModal.classList.add("hidden");
+        startModal.classList.remove("hidden");
+    }
 
     // adding click events to all of the cells here
     const addClickEvents = () => {
+        const startButton = document.querySelector("#start-button");
+        const restartButton = document.querySelector("#restart-button");
+        startButton.addEventListener("click", () => startGame());
+        restartButton.addEventListener("click", () => resetGame());
+
         for (let i = 0; i < game.gameBoard.length; i++) {
             cellList[i].addEventListener("click", () => {
                 if (game.getWinner() != null || game.gameBoard[i] != " ") {
-                    if (game.getWinner() != null) {
-                        endGame();
-                    }
                     return;
                 }
                 updateCell(i);
@@ -146,10 +210,9 @@ const handleDOM = (function() {
     };
     // GETTERS
 
-    return { cellList, updateCell, addClickEvents };
+    return { cellList, updateCell, addClickEvents, endGame, startGame, resetGame };
 })();
 
 
 // TESTING
 handleDOM.addClickEvents();
-
